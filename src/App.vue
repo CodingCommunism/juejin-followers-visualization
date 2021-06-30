b4a438b27bc415bfe69d9cb6011d6459d2e01cf1
gepingli <1435813129@qq.com>
2021-06-30 13:38:24 +0800

    feat: 完成开发

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <WordCloud />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import WordCloud from './components/word-cloud.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    WordCloud
   }
 })
 </script>
 
 <style>
+body {
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  alias:{
+    '/@': path.resolve(__dirname, 'src')
+  },
+  server: {
+    proxy: {
+      '/user_api/v1': {
+        target: 'https://api.juejin.cn/',
+        changeOrigin: true
+      }
+    }
+  }
 })
